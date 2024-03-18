84be1a0db700b9bd2f4a12813e17290f38db292f
Đorđe <xs55755@student.uni-lj.si>
2024-03-18 13:18:02 +0100

    Pokusaj da se popravi data fatching iz Produkcije

--- petmat/app/novosti/page.tsx
+++ petmat/app/novosti/page.tsx
@@ -3,6 +3,8 @@ import { listVesti } from "../lib/server/appwrite";
 import { Suspense } from "react";
 import Card from "../ui/cards";
 
+
+export const dynamic = 'force-dynamic';
 export default async function NovostiPage() {
     
 
